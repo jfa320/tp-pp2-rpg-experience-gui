a712ae388b3da63914115ee2d855007295dbfb0d
Fabián Juárez <juarefa320@gmail.com>
2023-08-23 19:39:26 -0300

    subo primera version del main

--- src/main/java/tp/pp2/rpg/generator/gui/Main.java
+++ src/main/java/tp/pp2/rpg/generator/gui/Main.java
@@ -4,6 +4,7 @@ import tp.pp2.rpg.generator.gui.view.VentanaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
+    	//AsignadorHabilidades asignadorHabilidades= new AsignadorHabilidades();
     	VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
     	ventanaPrincipal.setPanelInicial();
     }
